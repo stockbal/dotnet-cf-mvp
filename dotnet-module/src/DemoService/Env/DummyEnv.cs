70fd1bdbe28457e7a3f6641e69d52c9b85b7dc97
Ludwig Stockbauer-Muhr <Ludwig.Stockbauer-Muhr@msg.group>
2024-12-28 15:12:14 +0100

    refactor: simplify list initializer

--- dotnet-module/src/DemoService/Env/DummyEnv.cs
+++ dotnet-module/src/DemoService/Env/DummyEnv.cs
@@ -10,24 +10,24 @@ public class DummyEnv : IServiceEnv {
     private readonly List<UaaCredentials> _xsuaaCredentials;
 
     public DummyEnv() {
-        _xsuaaCredentials = new List<UaaCredentials>{
-                new() {
-                    ClientId = "sb-client-dotnet-demo",
-                    ClientSecret = "secret",
-                    Url = "https://auth.com",
-                    XsAppName = "dotnet-demo",
-                    VerificationKey = "BEGIN...END",
-                    UaaDomain = "authentication.com"
-                },
-                new() {
-                    ClientId = "sb-client-dotnet-demo2",
-                    ClientSecret = "secret",
-                    Url = "https://auth.com",
-                    XsAppName = "dotnet-demo",
-                    VerificationKey = "BEGIN...END",
-                    UaaDomain = "authentication.com"
-                }
-            };
+        _xsuaaCredentials = [
+            new() {
+                ClientId = "sb-client-dotnet-demo",
+                ClientSecret = "secret",
+                Url = "https://auth.com",
+                XsAppName = "dotnet-demo",
+                VerificationKey = "BEGIN...END",
+                UaaDomain = "authentication.com"
+            },
+            new() {
+                ClientId = "sb-client-dotnet-demo2",
+                ClientSecret = "secret",
+                Url = "https://auth.com",
+                XsAppName = "dotnet-demo",
+                VerificationKey = "BEGIN...END",
+                UaaDomain = "authentication.com"
+            }
+            ];
     }
 
     public List<UaaCredentials> XsuaaCredentials => _xsuaaCredentials;
